14f07ca9ca7c1a9ce810cf61d11eb4bb9ec7ebc3
Kasper Pickett <kasperpickett@gmail.com>
2018-02-02 06:08:17 -0600

    Assignment 10 completed

--- app.js
+++ app.js
@@ -2,19 +2,28 @@ function onReady() {
   const addToDoForm = document.getElementById('addToDoForm');
   const newToDoText = document.getElementById('newToDoText');
   const toDoList = document.getElementById('toDoList');
-    addToDoForm.addEventListener('submit', () => {
+    addToDoForm.addEventListener('submit', event => {
        event.preventDefault();
        let title = newToDoText.value;
        let newLi = document.createElement('li');
        let checkbox = document.createElement('input');
        checkbox.type = 'checkbox';
-       newLi.textContnt = title;
-       newLi.apppendChild(checkbox);
+       let del = document.createElement("button");
+       del.innerHTML = "Delete";
+       newLi.textContent = title;
+       newLi.appendChild(checkbox);
+       newLi.appendChild(del);
        toDoList.appendChild(newLi);
        newToDoText.value = '';
+
+       del.addEventListener("click", function() {
+        toDoList.removeChild(newLi)
+       });
      });
 
+
+}
+
 window.onload = function() {
-  alert("The window has loaded!");
-  onReady();
+ onReady();
 };

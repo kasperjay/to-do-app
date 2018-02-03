cbd47dd60860cfa1a7e6a3e507d614b4a849acdf
Kasper Pickett <kasperpickett@gmail.com>
2018-02-03 03:35:11 -0600

    checkpoint-11-DOMscripting2

--- app.js
+++ app.js
@@ -1,22 +1,46 @@
 function onReady() {
-  const addToDoForm = document.getElementById('addToDoForm');
-  const newToDoText = document.getElementById('newToDoText');
-  const toDoList = document.getElementById('toDoList');
-  const del = document.getElementById('del');
-  addToDoForm.addEventListener('submit', event => {
-       event.preventDefault();
-       let title = newToDoText.value;
-       let newLi = document.createElement('li');
-       let checkbox = document.createElement('input');
-       checkbox.type = 'checkbox';
-       newLi.textContent = title;
-       newLi.apppendChild(checkbox);
-       toDoList.appendChild(newLi);
+  let toDos = [];
 
-    del.addEventListener('submit', () => {
-      toDoList.removeChild(newLi);
-    }
-     });
+ const newToDoText = document.getElementById('newToDoText');
+
+ function createNewToDo() {
+    if (!newToDoText.value) { return; }
+
+    toDos.push({
+    title: newToDoText.value,
+    complete: false
+  });
+
+     newToDoText.value = '';
+
+     renderTheUI();
+   }
+
+  function renderTheUI() {
+    const toDoList = document.getElementById('toDoList');
+
+    toDoList.textContent = '';
+
+    toDos.forEach(function(toDo) {
+      const newToDo = document.createElement('li')
+      const checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
+
+      newLi.textContent = toDo.title;
+
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+    });
+
+  addToDoForm.addEventListener('submit', event => {
+    event.preventDefault();
+    createNewToDo();
+    newToDoText.value = '';
+  });
+
+  const addToDoForm = document.getElementById('addToDoForm');
+  const toDoList = document.getElementById('toDoList');
+}
 
 window.onload = function() {
   onReady();
